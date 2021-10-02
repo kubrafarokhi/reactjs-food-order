a80553cdcf197b68933754fd7961cdb99c39bd24
Qubra Farokhi <qubra.khutijatal@vedantu.com>
2021-10-02 12:49:06 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] =useState(false)
+
+  function showCartHandler(){
+    setCartIsShown(true)
+  }
+  function hideCardHandler(){
+    setCartIsShown(false)
+  }
   return (
     <CartProvider>
-      <Header />
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
