80976741f85c0913df49beda8b2b51190db0dc00
Kubra Farokhi <farokhi.kubra@gmail.com>
2021-10-02 14:17:30 +0530

    card popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
       <Header />
+      <Cart />
       <main>
         <Meals />
       </main>
